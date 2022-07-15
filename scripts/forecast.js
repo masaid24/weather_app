34bfd0582bdf07d0f81cdc98b0fedc04d0e30dd1
masaid24 <mahdisaid24@gmail.com>
2022-07-15 02:26:21 +0100

    First commit

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+const key = 'rC3D7RRtgNXvy8SqjA6DPNGPAhLQa3bJ';
+
+//get weather infos
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+};
+
+
+//get city infos
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
